f0efb1ba7a76886a4c83709a93c6340dacb13626
Nolen Johnson <johnsonnolen@gmail.com>
2017-10-19 22:40:35 -0400

    Revert "USB: xhci: correctly enable interrupts"
    This reverts commit fbac3befd79f49bbe89a3f67c2cd638d53aa6e6d.
    
    * T114 does XUSB and USB wakeups as a whole weird, revert
      this to fix USB.
    
    Reason: This breaks USB on the Google ADT-1 (molly).
    Change-Id: Ic82b2539cc9d74365993078434503170cc1b4b49

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -361,7 +361,7 @@ static int xhci_try_enable_msi(struct usb_hcd *hcd)
 	 * generate interrupts.  Don't even try to enable MSI.
 	 */
 	if (xhci->quirks & XHCI_BROKEN_MSI)
-		goto legacy_irq;
+		return 0;
 
 	/* unregister the legacy interrupt */
 	if (hcd->irq)
@@ -382,7 +382,6 @@ static int xhci_try_enable_msi(struct usb_hcd *hcd)
 		return -EINVAL;
 	}
 
- legacy_irq:
 	/* fall back to legacy interrupt*/
 	ret = request_irq(pdev->irq, &usb_hcd_irq, IRQF_SHARED,
 			hcd->irq_descr, hcd);
